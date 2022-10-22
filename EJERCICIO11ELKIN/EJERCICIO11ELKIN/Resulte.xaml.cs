d123a2719158a159b3648dd9f3e1d7198e701ee4
ElkYTHN <elkinportillo4@gmail.com>
2022-10-21 19:41:27 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIO11ELKIN/EJERCICIO11ELKIN/Resulte.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EJERCICIO11ELKIN
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resulte : ContentPage
+    {
+        public Resulte()
+        {
+            InitializeComponent();
+        }
+    }
+}
